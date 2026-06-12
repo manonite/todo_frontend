8634dcee61d70d8ea05f78048a0d3be3e10dc451
manonite <manonidhalle@gmail.com>
2026-06-12 11:12:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [text, setText] = useState("");
+  const fetchNotes = async () => {
+    const res = await axios.get(`${import.meta.env.VITE_URI}/todo/get`);
+    setNotes(res.data);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  const addNote = async () => {
+    await axios.post(`${import.meta.env.VITE_URI}/todo`, { text });
+    setText("");
+    fetchNotes();
+  };
+  const deleteNote = async (id) => {
+    await axios.delete(`${import.meta.env.VITE_URI}/todo/:id`);
+    fetchNotes();
+  };
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Notes App</h2>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter your note"
+      />
+      <button onClick={addNote}>Add</button>
+      <ol>
+        {notes && notes.map((note) => (
+          <li key={note._id}>
+            {note.text}{" "}
+            <button onClick={() => deleteNote(note._id)}>Delete</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+export default App;
